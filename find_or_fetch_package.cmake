ba48e1661ea57da57881fd7fd49bd5851e7d3f5c
Rafał Lalik <rafallalik@gmail.com>
2023-03-07 14:17:11 +0100

    Add subdirs if cmake file exists

--- find_or_fetch_package.cmake
+++ find_or_fetch_package.cmake
@@ -6,7 +6,8 @@ macro(find_or_fetch_package name url)
   set(args_mult "")
   cmake_parse_arguments(ARG "${options}" "${args}" "${args_mult}" ${ARGN})
 
-  string(TOUPPER ${name} upper_case_name)
+  string(TOUPPER ${name} ucName)
+  string(TOLOWER ${name} lcName)
 
   # git tag (branch, tag, etc) may be different than version name, even due to
   # semantics
@@ -16,8 +17,8 @@ macro(find_or_fetch_package name url)
     set(GIT_TAG ${ARG_VERSION})
   endif()
 
-  if(USE_BUILTIN_${upper_case_name})
-    set(DEFAULT ${USE_BUILTIN_${upper_case_name}})
+  if(USE_BUILTIN_${ucName})
+    set(DEFAULT ${USE_BUILTIN_${ucName}})
   elseif(ARG_FIND)
     set(DEFAULT FIND)
   elseif(ARG_FETCH)
@@ -27,27 +28,27 @@ macro(find_or_fetch_package name url)
   endif()
 
   if (ARG_EXPORT_OPTION)
-    set(USE_BUILTIN_${upper_case_name}
+    set(USE_BUILTIN_${ucName}
       ${DEFAULT}
       CACHE STRING "Fetch or find ${name}")
-    set_property(CACHE USE_BUILTIN_${upper_case_name} PROPERTY STRINGS AUTO;FIND;FETCH)
+    set_property(CACHE USE_BUILTIN_${ucName} PROPERTY STRINGS AUTO;FIND;FETCH)
   endif()
 
   if(DEFAULT STREQUAL AUTO)
     find_package(${name} ${ARG_VERSION} QUIET)
     if(NOT ${name}_FOUND)
-      set(USE_BUILTIN_${upper_case_name} TRUE)
+      set(USE_BUILTIN_${ucName} TRUE)
     endif()
   elseif(DEFAULT STREQUAL FIND) # a true value (such as ON) was used
-    set(USE_BUILTIN_${upper_case_name} FALSE)
+    set(USE_BUILTIN_${ucName} FALSE)
     find_package(${name} ${version} REQUIRED)
   elseif(DEFAULT STREQUAL FETCH) # a false value (such as OFF) was used
-    set(USE_BUILTIN_${upper_case_name} TRUE)
+    set(USE_BUILTIN_${ucName} TRUE)
   else()
-    message(FATAL_ERROR "No FETCH/FIND/AUTO mode selected. Use ${USE_BUILTIN_${upper_case_name}} or set proper mode")
+    message(FATAL_ERROR "No FETCH/FIND/AUTO mode selected. Use ${USE_BUILTIN_${ucName}} or set proper mode")
   endif()
 
-  if(USE_BUILTIN_${upper_case_name})
+  if(USE_BUILTIN_${ucName})
 
     # We need to know either version or tag to fetch proper branch
     if ("${GIT_TAG}" STREQUAL "")
@@ -63,16 +64,18 @@ macro(find_or_fetch_package name url)
     # FetchContent_MakeAvailable requires CMake-3.14 or newer
     if(CMAKE_VERSION VERSION_LESS 3.14)
       FetchContent_GetProperties(${name})
-      if(NOT ${name}_POPULATED)
+      if(NOT ${lcName}_POPULATED)
         FetchContent_Populate(${name})
-        add_subdirectory(${${name}_SOURCE_DIR} ${${name}_BINARY_DIR})
+        if(EXISTS ${${lcName}_SOURCE_DIR}/CMakeLists.txt)
+          add_subdirectory(${${lcName}_SOURCE_DIR} ${${lcName}_BINARY_DIR})
+        endif()
       endif()
     else()
       FetchContent_MakeAvailable(${name})
     endif()
 
   else()
-    message(STATUS "Uses system-provided ${name}")
+    message(STATUS "Uses system-provided ${name} ${ARG_VERSION}")
   endif()
 
 endmacro()
